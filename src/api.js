d92643dd9ce394640fce1abcc2a99c52d582dfc9
aelpires <aelpires@gmail.com>
2023-05-26 19:36:17 +0100

    first commit

---
+++ src/api.js
@@ -0,0 +1,11 @@
+ export const geoApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '44e812c1d1msh3ef3f1f505f6918p1d89f1jsne1f210d5436b',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	},
+};
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "48dbc5238f81df17ea4ba77a5b57e3fb";
